48dc6e459bfd5c38d1629565413c4dc985ca067a
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2026-01-04 02:41:08 +0530

    Get products

---
+++ src/main/java/org/example/storeapplication/Product.java
@@ -0,0 +1,84 @@
+package org.example.storeapplication;
+
+import java.util.Objects;
+
+public class Product  {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private Integer categoryId;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, String description, Double price, Integer quantity, Integer categoryId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryId = categoryId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(description, product.description) && Objects.equals(price, product.price) && Objects.equals(quantity, product.quantity) && Objects.equals(categoryId, product.categoryId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, price, quantity, categoryId);
+    }
+}
